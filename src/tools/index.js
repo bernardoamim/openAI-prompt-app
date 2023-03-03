08ef6be471aaa22c1cdd7dd4c732221bd71111f6
Bernardo Amim <bernardoamim@m1-pro2.local>
2023-03-03 13:20:06 -0300

    first commit

---
+++ src/tools/index.js
@@ -0,0 +1,19 @@
+import jobad from './jobad';
+import summarize from './summarize';
+import codeInterpret from './interpret';
+import intro from './intro';
+import helloworld from './helloworld';
+import instagrampost from './instagrampost';
+
+const TOOLS = [
+
+	codeInterpret,
+	intro,
+	jobad,
+	summarize,
+	helloworld,
+	instagrampost,
+
+]
+
+export default TOOLS
